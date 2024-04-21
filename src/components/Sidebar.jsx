3421d919d1c124a341159af7ea92572bee77fc7a
jorgesoares2997 <jorgesoares2997@gmail.com>
2024-04-21 00:21:29 -0300

    curriculo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import avatar from '../img/jorgin.jpg'
-
+import curriculo from '../curriculo/Jorge Soares.pdf'
 import "../styles/components/sidebar.sass"
 import InformationContainer from './InformationContainer'
 import SocialNetworks from './SocialNetworks'
@@ -12,7 +12,7 @@ const Sidebar = () => {
         <p className="title">Desenvolvedor Junior</p>
         <SocialNetworks />
         <InformationContainer/>
-        <a href="#" className="btn">Download Curriculo</a>
+        <a href={curriculo} className="btn">Download Curriculo</a>
       </aside>
     </div>
   )
